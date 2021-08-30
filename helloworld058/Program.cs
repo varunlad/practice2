0429911b4e77d5b66d03b066f856b39213bd7f48
varunlad <varunlad5@gmail.com>
2021-08-30 15:39:54 +0530

    10 Times Hellow World

---
+++ helloworld058/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloworld058
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for ( i = 1 ; i < 11; i++)
+            {
+                Console.WriteLine(" {0} : Hellow world  ", i);
+               
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
